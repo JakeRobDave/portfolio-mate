48ecd83984d45605525b401e912ada99a98f8f8e
Jake Adamson <Jake-The-Human@users.noreply.github.com>
2017-12-05 17:22:23 -0500

    .

--- js/imgLogic.js
+++ js/imgLogic.js
@@ -6,3 +6,15 @@ $(document).ready(function(){
     $(this).append("<img src='"+folder+(i)+".png' width='79' height='79' />");
   });
 });
+
+
+$.ajax({
+    url : folder,
+    success: function (data) {
+        $(data).find("a").attr("href", function (i, val) {
+            if( val.match(/\.(jpe?g|png|gif)$/) ) {
+                $("body").append( "<img src='"+ folder + val +"'>" );
+            }
+        });
+    }
+});
